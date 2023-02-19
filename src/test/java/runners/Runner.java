9ab775c51593ca845c74f29bf52ff364ef70f78d
Adem Yusuf <adiguzelademyusuf@gmail.com>
2023-02-19 23:49:21 +0300

    02/19/2023

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,14 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "./src/test/resources/features",
-        glue = "stepdefinition"//stepdefinitions path
+        features = "./src/test/resources/features",//features folder path
+        glue = "stepdefinitions",//stepdefinitions path
+        tags = "@iphone or @tesla"
 )
 public class Runner {
-
-
-
 }
-
-//Bu sinif Test caseleri Run etmek icin kullanilir
+//Bu sinif Test caseleri RUN etmek icin kullanilir
 //Ve konfigurasyonlar icin kullanilir
