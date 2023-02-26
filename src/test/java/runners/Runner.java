e5471eeac8837584a9aa73703841040533837469
Adem Yusuf <adiguzelademyusuf@gmail.com>
2023-02-26 23:54:26 +0300

    02/26/2023

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -4,10 +4,18 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {
+                "pretty",//raporlarin daha okunakli olmasi icin
+                "html:target/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"
+        },
+        monochrome = true,
         features = "./src/test/resources/features",//features folder path
         glue = "stepdefinitions",   //stepdefinitions path
         tags = "@personel_olusturma",
         dryRun = false
+
 )
 public class Runner {
 }
